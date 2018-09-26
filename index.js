6274bc90660b7e02747c5f43a469b26e54d6cf8a
Samuel Horton <sam.horton@kyndi.com>
2018-09-26 08:29:29 -0700

    chore: format with prettier

--- index.js
+++ index.js
@@ -4,16 +4,37 @@ var createLogger = function createLogger(backgroundColor, color) {
   var logger = function logger(message) {
     var _console;
 
-    for (var _len = arguments.length, args = Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+    for (
+      var _len = arguments.length,
+        args = Array(_len > 1 ? _len - 1 : 0),
+        _key = 1;
+      _key < _len;
+      _key++
+    ) {
       args[_key - 1] = arguments[_key];
     }
 
-    if (logger.enabled === false) {
-      return;
-    }
+    if (logger.enabled === false) return;
 
-    (_console = console).groupCollapsed.apply(_console, ["%c" + message, "background-color: " + backgroundColor + "; color: " + color + "; padding: 2px 4px;"].concat(args));
-    console.log(new Error("stack").stack.split("\n").slice(2).join("\n"));
+    (_console = console).groupCollapsed.apply(
+      _console,
+      [
+        "%c" + message,
+        "background-color: " +
+          backgroundColor +
+          "; color: " +
+          color +
+          "; padding: 2px 4px;",
+      ].concat(args)
+    );
+
+    console.log(
+      new Error("stack").stack
+        .split("\n")
+        .slice(2)
+        .join("\n")
+    );
+
     console.groupEnd();
   };
 
@@ -28,7 +49,7 @@ var log = {
   red: createLogger("#E53935", "#EF9A9A"),
   orange: createLogger("#F4511E", "#FFAB91"),
   purple: createLogger("#8E24AA", "#E1BEE7"),
-  yellow: createLogger("#FFD600", "#FFF59D")
+  yellow: createLogger("#FFD600", "#FFF59D"),
 };
 
 module.exports = log;
